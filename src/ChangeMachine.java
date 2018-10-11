176bffb2c550e461549ed640e46e7b25012b5df7
CharlieSwires <charlieswires@gmail.com>
2018-10-11 18:47:56 +0100

    Initial

---
+++ src/ChangeMachine.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ChangeMachine {
+
+	static final int[] coins = {200,100,50,20,10,5,2,1};
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<Integer> change(int i) {
+		int left = i;
+		ArrayList<Integer> changeList = new ArrayList<>();
+		for(;left != 0;) {
+			for(int coin:coins) {
+				if(coin <= left) {
+					left -= coin;
+					changeList.add(coin);
+					break;
+				}
+			}
+		}
+		return changeList;
+	}
+
+	public List<Integer> change(List<Integer> c, int i) {
+		ArrayList<Integer> coins = new ArrayList<Integer>();
+		for(Integer coin:c) {
+			coins.add(coin);
+		}
+		coins.sort(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				//descending order
+				return o2.compareTo(o1);
+
+			}
+		}
+				);
+		int left = i;
+		ArrayList<Integer> changeList = new ArrayList<>();
+		for(;left != 0;) {
+			for(int index = 0; index < coins.size(); index++) {
+				int coin = coins.get(index);
+				if(coin <= left) {
+					left -= coin;
+					coins.remove(index);
+					changeList.add(coin);
+					break;
+				}
+			}
+		}
+
+		return changeList;
+	}
+
+}
---
+++ src/ChangeMachineTests.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+class ChangeMachineTests {
+
+	@Test
+	void test1() {
+		ChangeMachine changeMachine = new ChangeMachine();
+		List<Integer> change = changeMachine.change(13);
+		int[] array = {10,2,1};
+		List<Integer> expectedResult = toList(array);
+		Assert.assertTrue("13 = 10,2,1", change.equals(expectedResult));
+	}
+	@Test
+	void test2() {
+		ChangeMachine changeMachine = new ChangeMachine();
+		List<Integer> change = changeMachine.change(574);
+		int[] array = {200, 200, 100, 50, 20, 2, 2};
+		List<Integer> expectedResult = toList(array);
+		Assert.assertTrue("574 = 200, 200, 100, 50, 20, 2, 2", change.equals(expectedResult));
+	}	
+	@Test
+	void test3() {
+		ChangeMachine changeMachine = new ChangeMachine();
+		List<Integer> change = changeMachine.change(Arrays.asList(200, 100, 50, 20, 10, 5, 2, 1), 13);
+		int[] array = {10,2,1};
+		List<Integer> expectedResult = toList(array);
+		Assert.assertTrue("13 = 10,2,1", change.equals(expectedResult));
+	}	
+	@Test
+	void test4() {
+		ChangeMachine changeMachine = new ChangeMachine();
+		List<Integer> change = changeMachine.change(Arrays.asList(200, 100, 20, 200, 10, 5, 50, 2, 1, 1, 2), 574);
+		int[] array = {200, 200, 100, 50, 20, 2, 2};
+		List<Integer> expectedResult = toList(array);
+		Assert.assertTrue("574 = 200, 200, 100, 50, 20, 2, 2", change.equals(expectedResult));
+	}
+
+	static List<Integer> toList(int[] array){
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		for(Integer i:array) {
+			arrayList.add(i);
+		}
+		return (List)arrayList;
+	}
+
+}
